25e5bdc18950b4bf393b9fe7fc6d60ce8c46332a
jreyes <josereyes5396@outlook.com>
2016-04-25 19:05:06 -0400

    First regex version

---
+++ emailRegex.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import re
+
+regex = r"(\w+(\.?\w+)*(\w+)?)\+?\w*@(\w+(\.\w+)?\.(com|org|net))"
+
+def emailCheck (email):
+    if re.search(regex, email):
+        match = re.search(regex, email)
+        print "Congrats! Your email checks out.\nYour username is %s, and your domain is %s" % (match.group(1), match.group(2))
+    else:
+        print "Please, enter a valid email."
+
+email = raw_input("Enter your email: ")
+
+emailCheck(email)
