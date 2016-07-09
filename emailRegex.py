b6fd04d61f23d8fb94afc8abac6f6e878aefbfc6
jreyes <josereyes5396@outlook.com>
2016-07-09 01:09:31 -0400

    Split success results into two lines

--- emailRegex.py
+++ emailRegex.py
@@ -7,7 +7,8 @@ regex = r"(\w+(\.?\w+)*(\w+)?)\+?\w*@(\w+(\.?\w+?)*\.(com|org|net|edu))"
 def emailCheck (email):
     if re.search(regex, email):
         match = re.search(regex, email)
-        print "Congrats! Your email checks out.\nYour username is %s, and your domain is %s" % (match.group(1), match.group(4))
+        print "Congrats! Your email checks out."
+        print "Your username is %s, and your domain is %s" % (match.group(1), match.group(4))
     else:
         print "Please, enter a valid email."
 
